5b386454b1ed18c6870187ad1ed49b02e6001b08
vaskipet <pvaskin1@gmail.com>
2017-11-08 21:21:38 +0200

    commenting

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// basically every function in this small app begins by defining consts
+// basically every function in this small app begins by defining variables
 // then adding an evet to them. 
 // the app has simple functions including adding items, deleting items,
 // filtering (or searching) and hiding all of them.
@@ -6,11 +6,12 @@
 // making sure that the app.js works
 document.addEventListener('DOMContentLoaded', function() {
 
-    // building consts before adding functionalities
-    const listItem = document.querySelector('#todo-list ul');
-    const addForm = document.forms['add-todo'];
-    const hideBox = document.querySelector('#hide');
-    const searchBar = document.forms['search-todos'].querySelector('input');
+    // building global vars before adding functionalities
+    var listItem = document.querySelector('#todo-list ul');
+    var addForm = document.forms['add-todo'];
+    var hideBox = document.getElementById('hide');
+    var searchBar = document.forms['search-todos'].querySelector('input');
+    var field = document.getElementById('myInput');
 
     //once user submits something, addTodo is fired
     addForm.addEventListener('submit', addTodo);
@@ -24,27 +25,26 @@ document.addEventListener('DOMContentLoaded', function() {
     //adding todos
     function addTodo(e) {
         e.preventDefault(); //so the page does not reload when clicked!
-        const value = addForm.querySelector('input[type="text"]').value;
+        var value = addForm.querySelector('input[type="text"]').value;
 
         // adding some form validation so that the user enters at least two character to the list
-        if (value == null || value == '' || value.length < 2) {
+        if (value == null || value == '' || value.length < 3) {
             alert('Please fill the form in order to add a todo!');
-            var field = document.getElementById('myInput');
+            //adding a css-class to the input field
             field.setAttribute('class', 'warning');
         } else {
             //in order to add new items we need to create elements it consists of
-            var field = document.getElementById('myInput');
             field.setAttribute('class', 'form-control');
-            const li = document.createElement('li');
-            const todoName = document.createElement('span');
-            const deleteBtn = document.createElement('span');
+            var li = document.createElement('li');
+            var todoName = document.createElement('span');
+            var deleteBtn = document.createElement('span');
 
             //add the delete icon and the value that was written by user
             deleteBtn.innerHTML = "<i class='fa fa-trash'>";
             todoName.textContent = value;
 
             //add classes to the todo
-            todoName.classList.add('name');
+            // todoName.classList.add('name');
             deleteBtn.classList.add('delete');
             li.classList.add('list-group-item');
 
@@ -56,32 +56,32 @@ document.addEventListener('DOMContentLoaded', function() {
             //the li is appeded to the list
             listItem.appendChild(li);
 
+            //after input we want to clear the form
             document.getElementById("myInput").value = "";
-            // document.getElementById("myInput").className -= "green";
         }
     };
 
     //deleting todos
     function deleteTodos(e) {
         if (e.target.className === 'delete' || e.target.className === 'fa-trash') {
-            const li = e.target.parentElement;
+            var li = e.target.parentElement;
             listItem.removeChild(li);
         }
     };
 
     //hiding todos
     function hideTodo() {
-        //if the checkbox is checked then don´t diplay the list. else diplay as block-
+        //if the checkbox is checked then don´t diplay the list. else diplay as block.
         hideBox.checked ? listItem.style.display = "none" : listItem.style.display = "block";
     };
 
     // filter or search function 
     function filterTodos(e) {
         //grabbing the list of the todos
-        const todoItems = listItem.getElementsByTagName('li');
-        //turn todoItems into array so we can use forEach to iterate through all todos
+        var todoItems = listItem.getElementsByTagName('li');
+        //turn todoItems into array so we can use forEach to iterate through all todos.
         Array.from(todoItems).forEach((item) => {
-            const val = item.firstElementChild.textContent;
+            var val = item.firstElementChild.textContent;
             //if-else: if the value has index value then display block, else don´t display. 
             val.indexOf(e.target.value) != -1 ? item.style.display = "block" : item.style.display = "none";
         });
